122443dfcbe190e1671d926def4c4208e2eb51b8
Witalij Berdinskich <radio_rogal@keemail.me>
2022-09-25 23:21:02 +0300

    Add slf4j2-mock - service provider for 2.0.x

---
+++ slf4j2-mock/src/main/java/org/simplify4u/slf4jmock/MockServiceProvider.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2022 Slawomir Jaranowski
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.simplify4u.slf4jmock;
+
+import org.slf4j.ILoggerFactory;
+import org.slf4j.IMarkerFactory;
+import org.slf4j.helpers.BasicMarkerFactory;
+import org.slf4j.spi.MDCAdapter;
+import org.slf4j.spi.SLF4JServiceProvider;
+
+public class MockServiceProvider implements SLF4JServiceProvider {
+
+    /**
+     * Declare the version of the SLF4J API this implementation is compiled against. The value of
+     * this field is modified with each major release.
+     */
+    // to avoid constant folding by the compiler, this field must *not* be final
+    public static String REQUESTED_API_VERSION = "2.0.99"; // !final
+
+    private final ILoggerFactory loggerFactory = LoggerMock.getLoggerFactory();
+    private final IMarkerFactory markerFactory = new BasicMarkerFactory();
+    private final MDCAdapter contextAdapter;
+
+    public MockServiceProvider() {
+        contextAdapter = new MDCMock().getMock();
+    }
+
+    @Override
+    public ILoggerFactory getLoggerFactory() {
+        return loggerFactory;
+    }
+
+    @Override
+    public IMarkerFactory getMarkerFactory() {
+        return markerFactory;
+    }
+
+    @Override
+    public MDCAdapter getMDCAdapter() {
+        return contextAdapter;
+    }
+
+    @Override
+    public String getRequestedApiVersion() {
+        return REQUESTED_API_VERSION;
+    }
+
+    @Override
+    public void initialize() {
+        // do nothing
+    }
+}
